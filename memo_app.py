d72f9c713069851899e5159f446d6e8f0bbf6e91
yosino26 <fn.yoshino57@gmail.com>
2026-07-04 23:05:50 +0900

    中身はまだ作らず、メニューだけ作成1

--- memo_app.py
+++ memo_app.py
@@ -1,2 +1,13 @@
-print("メモ管理アプリを開始します")
-print("メモ管理アプリを開始します")
+defshow_menu():
+print("==== メモ管理アプリ ====")
+print("1. メモを追加")
+print("2. メモ一覧を見る")
+print("3. メモを検索")
+print("4. メモを削除")
+print("5. 終了")
+
+
+whileTrue:
+show_menu()
+
+choice=input("番号を選んでください: ")
